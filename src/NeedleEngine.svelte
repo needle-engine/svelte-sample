a6b261498faedabced8aaabf37873ae203211036
hybridherbst <felix.herbst@gmail.com>
2026-03-05 17:47:31 +0100

    update to Svelte 5

--- src/NeedleEngine.svelte
+++ src/NeedleEngine.svelte
@@ -1,23 +1,20 @@
 <script lang="ts">
+    import type { Snippet } from 'svelte';
 
     /**
      * The src can be a remote url to a GLB (or local file)
     */
-    export let src: string | undefined = undefined;
-
-    /**
-     * Skybox Image can be assigned via a URL e.g. https://dl.polyhaven.org/file/ph-assets/HDRIs/exr/2k/tief_etz_2k.exr
-    */
-    export let skyboxImage: string | undefined = undefined;
-    /**
-     * Environment Image can be assigned via a URL e.g. https://dl.polyhaven.org/file/ph-assets/HDRIs/exr/2k/tief_etz_2k.exr
-    */
-    export let environmentImage: string | undefined = undefined;
+    let { src, skyboxImage, environmentImage, children }: {
+        src?: string;
+        skyboxImage?: string;
+        environmentImage?: string;
+        children?: Snippet;
+    } = $props();
 </script>
 
 <!-- find all attributes in the documentation at https://engine.needle.tools/docs/reference/needle-engine-attributes.html-->
-<needle-engine {src} skybox-image="{skyboxImage}" environment-image="{environmentImage}">
-    <slot />
+<needle-engine {src} skybox-image={skyboxImage} environment-image={environmentImage}>
+    {@render children?.()}
 </needle-engine>
 
 <style>
