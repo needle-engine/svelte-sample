c162a0f720dc90f0d8997c9509b4db5b4cef9650
Marcel Wiessler <marcel@gaisterhand.de>
2026-03-19 17:40:50 +0700

    fix template + include codegen

---
+++ src/generated/gen.js
@@ -0,0 +1,24 @@
+﻿/*
+* Developer Information:
+* This file was generated using Unity 2021.3.45f2.
+* Do not modify this file manually.
+
+* Instead of using generated code you can also load the 3D scene like this: <needle-engine src="/assets/your_glTF_name.glb"></needle-engine>
+* (When you're working with Unity the glTF file name will always match your scene or prefab's name)
+*/
+
+globalThis["needle:dependencies:ready"] = import("./register_types.ts")
+
+export const needle_exported_files = new Array();
+globalThis["needle:codegen_files"] = needle_exported_files;
+import "../../assets/CollaborativeSandbox.glb?url";
+needle_exported_files.push("/assets/CollaborativeSandbox.glb");
+document.addEventListener("DOMContentLoaded", () =>
+{
+	const needleEngine = document.querySelector("needle-engine");
+	if(needleEngine && needleEngine.getAttribute("src") === null)
+	{
+		needleEngine.setAttribute("hash", "1773916664482");
+		needleEngine.setAttribute("src", JSON.stringify(needle_exported_files));
+	}
+});
---
+++ src/generated/register_types.ts
@@ -0,0 +1,4 @@
+﻿// Import types from dependencies
+import "@needle-tools/samples-scripts/codegen/register_types.ts"
+
+export {}
